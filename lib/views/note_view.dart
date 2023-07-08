3bd3a0a874d8163467814b5d3336c8030f001cae
MSa1ah <mohamedsalah.xv80@gmail.com>
2023-07-08 13:01:45 +0300

    change theme and build note niew

---
+++ lib/views/note_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Note_view extends StatelessWidget {
+    const Note_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
